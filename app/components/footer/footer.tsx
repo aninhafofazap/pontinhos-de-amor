32ceb12edab185dda1782b5b8ffcb4ae78695125
Ana <anavictoriagomes1810@gmail.com>
2024-06-10 23:04:42 -0300

    Finished styling on the footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -15,7 +15,7 @@ function Footer() {
                 <div className="sections">
                     <h3>Contato</h3>
                     <p>55 999999999</p>
-                    <p>pontinhos@email.com</p>
+                    <p>Petani.info@email.com</p>
                 </div>
                 <div className="sections">
                     <h3>Bolsas</h3>
@@ -31,6 +31,9 @@ function Footer() {
                     <h3>Endereço</h3>
                     <p>Brasil Rua 2 Senador Canedo Anapolis Goias 750000</p>
                 </div>
+            </div>
+            <div className="diretory">
+                <p className="diretory-info">© 2024 Pontinhos de Amor - Todos os direitos reservados.</p>
             </div>
         </div>
     )
