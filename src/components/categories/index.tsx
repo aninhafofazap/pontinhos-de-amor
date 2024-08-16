5d1327be7cef074dbb63157aca060fd3e4ecf8f7
Ana Victoria <anavictoriagomes1810@gmail.com>
2024-08-15 22:58:57 -0300

    .

--- src/components/categories/index.tsx
+++ src/components/categories/index.tsx
@@ -6,59 +6,48 @@ export const Categories = () => {
   return (
     <section className="categories">
       <div className="categories-wrapper">
-        <h2 className="subtitle-category">Catálago de produtos</h2>
+        <h2 className="categories-subtitle">Catálago de produtos</h2>
 
         {/* List */}
-        <div className="col-category">
-          <div className="bags-category">
-            <Image
+        <div className="categories-col">
+          <div className="categories-col-bags">
+            <img
               className="bags-img"
               src="https://http2.mlstatic.com/D_NQ_NP_999572-MLB48633503036_122021-O.webp"
-              width={100}
-              height={100}
-              alt=""
             />
             <p className="description-category">Dia a dia </p>
           </div>
-          <div className="bags-category">
-            <Image
+          <div className="categories-col-bags">
+            <img
               className="bags-img"
               src="https://http2.mlstatic.com/D_NQ_NP_999572-MLB48633503036_122021-O.webp"
-              width={100}
-              height={100}
-              alt=""
             />
             <p className="description-category">Festas</p>
           </div>
-          <div className="bags-category">
-            <Image
+          <div className="categories-col-bags">
+            <img
               className="bags-img"
               src="https://http2.mlstatic.com/D_NQ_NP_999572-MLB48633503036_122021-O.webp"
-              width={100}
-              height={100}
-              alt=""
             />
             <p className="description-category">Praia</p>
           </div>
-          <div className="bags-category">
-            <Image
+          <div className="categories-col-bags">
+            <img
               className="bags-img"
               src="https://http2.mlstatic.com/D_NQ_NP_999572-MLB48633503036_122021-O.webp"
-              width={100}
-              height={100}
-              alt=""
             />
             <p className="description-category">Sustentaveis</p>
           </div>
         </div>
-        {/* End List */}
-        <div className="link-category">
+        <div className="categories-link">
           <div className="link-container">
             <IoArrowForward />
             <a href="/catalog">Ver todo o catálogo</a>
           </div>
         </div>
       </div>
+
+      {/* End List */}
     </section>
   );
 };
